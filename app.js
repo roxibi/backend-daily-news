0e3e7bc3c090de47b03c491b5012ad6573e95bc8
Roxana Bita <roxana.bita@gmail.com>
2023-02-20 16:42:32 +0100

    calling api

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
-import cors from 'cors'
 
-const express = require('express')
+
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const dotenv = require('dotenv').config();
+
 
 const app = express()
 const port = 5000
 
+// const query="top-headlines?country=de";
+
+
+
 app.use(cors());
 app.use(express.json());
-app.use('*', (req, res)=>{appres.status(404).json({error:"not found"})})
+// app.use('*', (req, res)=>{res.status(404).json({error:"page not found"})})
+
+
+
+app.get('/news/:query', async (req, res) => {
+  try {
+    console.log(req);
+    const query=decodeURIComponent(req.params.query);
+    const response = await axios.get(`https://newsapi.org/v2/${query}&apiKey=${process.env.NEWS_API_KEY}`);
+    const news = response.data.articles;
+    res.json(news);
+  } catch (error) {
+    console.error(error, `https://newsapi.org/v2/${req.params.query}&apiKey=${process.env.NEWS_API_KEY}`);
+    res.status(500).send('Server error');
+  }
+});
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.get('/tech', (req, res) => {
     res.send('Hello World!')
   })
 
   app.get('/bbc', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello bbc!')
   })
 
 app.listen(port, () => {
   console.log(`app listening on port ${port}`)
 })
 
-export default app
---
+++ routes/news.js
@@ -0,0 +1,8 @@
+// import express, express.Router() from 'express';
+// const newsRouter = express.Router()
+
+// newsRouter.get('', async(req, res)=>{res.render('news')})
+
+
+// module.exports=newsRouter
+
